26949fa1a186b0e1226d60a868923c97705941c1
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-01 01:24:17 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/pharmacyapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/pharmacyapp/ui/MainActivity.kt
@@ -2,17 +2,17 @@ package com.example.pharmacyapp.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pharmacyapp.R
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // مثال على التنقل بين الواجهات
-        findViewById<Button>(R.id.btnMedicines).setOnClickListener {
+        val button = findViewById<Button>(R.id.btnMedicines)
+        button.setOnClickListener {
             startActivity(Intent(this, MedicineListActivity::class.java))
         }
     }
